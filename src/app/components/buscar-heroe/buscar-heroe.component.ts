0f260f439a7621d91d0d17bd9b930a12bcccaea5
miguel_casas <miguel.casas@atmira.com>
2020-05-20 19:08:41 +0200

    Solucionado que el buscador solo funcionaba la primera vez

--- src/app/components/buscar-heroe/buscar-heroe.component.ts
+++ src/app/components/buscar-heroe/buscar-heroe.component.ts
@@ -10,24 +10,21 @@ export class BuscarHeroeComponent implements OnInit {
 
   termino: string;
   
-  heroes: Heroe[];
+  heroes: Heroe[] = [];
   
   constructor( private activatedRoute: ActivatedRoute,
                private _heroesService: HeroesService,
                private router: Router) {
-
-    this.activatedRoute.params.subscribe( params => {
-      this.termino = params['termino'];
-      console.log('buscar-heroe -> constructor ' + this.termino);
-    } );
-
-   }
+  }
 
   ngOnInit(): void {
 
-    console.log('buscar-heroe -> ngOnInit');
-    this.heroes = this._heroesService.buscarHeroes(this.termino);
-    console.log(this.heroes);
+    this.activatedRoute.params.subscribe( params => {
+        this.termino = params['termino'];
+        console.log('buscar-heroe -> ngOnInit');
+        this.heroes = this._heroesService.buscarHeroes(this.termino);
+        console.log(this.heroes);
+      } );
 
   }
 
